b9c7aef3b1025acb507b28820dec51528403df90
valeriaaa12 <valeriaromero1275@gmail.com>
2024-11-26 23:22:08 -0600

    ssfwe

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 const axios = require('axios');
 const path = require('path');
 const port = 3000;
-
-
+const { connect } = require('./config/db');
+const dotenv = require('dotenv');
+connect();
