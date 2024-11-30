a5b6f6e3e0b13d2f38c3fe69d6a424414e9cf4ad
valeriaaa12 <valeriaromero1275@gmail.com>
2024-11-30 11:19:31 -0600

    firebase login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,29 @@ const {getDB} = require('../config/db')
 const dotenv = require('dotenv')
 
 dotenv.config()
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase.js";
 
-//Register
-/*
-1. Verificar si ya hay un usuario creado
-2. Crear un usuario
-*/
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    res.status(201).json({ message: "Usuario registrado exitosamente", user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-// Login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    res.status(200).json({ message: "Login exitoso", user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -2,9 +2,27 @@
 const axios = require('axios');
 const path = require('path');
 const port = 3000;*/
-const { connect } = require('./config/db');
-const dotenv = require('dotenv');
+const express = require('express'); 
+const dotenv = require('dotenv'); 
+const { connect } = require('./config/db'); 
+const authRoutes = require('./routes/auth'); 
 
-
+dotenv.config();
 
 connect();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/auth', authRoutes); 
+
+app.get('/', (req, res) => {
+  res.send('Servidor corriendo correctamente.');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
